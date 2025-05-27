720e027d2b9b5883bbec441abbcf4dfa7b252fab
vaibhav singh <115387277+vaibhav163@users.noreply.github.com>
2025-05-27 12:40:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function highlight() {
-    //Write your code here
-
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
-    
-}
+const link=document.getElementById("link");
+  const str=document.querySelectorAll("strong");
+  function highlight(){
+    str.forEach(el=>{
+    el.style.color='rgb(0, 128, 0)';
+  });
+  }
+  function return_normal(){
+    str.forEach(el=>{
+    el.style.color="rgb(0, 0, 0)";
+  });
+  }
